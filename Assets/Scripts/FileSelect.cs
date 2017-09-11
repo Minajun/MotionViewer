c397998b056076340af7add23ba7a4c7e9df1eb1
Minajun <s1620678@u.tsukuba.ac.jp>
2017-09-11 22:02:32 +0900

    hoge

---
+++ Assets/Scripts/FileSelect.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FileSelect : MonoBehaviour {
+Vector2 scrollPosition = Vector2.zero;
+   public int selGridInt = 0;
+    public string[] selStrings = new string[] {"radio1"};
+    void OnGUI(){
+        scrollPosition = GUILayout.BeginScrollView(scrollPosition, false, true,  GUILayout.Width(400),  GUILayout.Height(300)); 
+       
+        selGridInt = GUILayout.SelectionGrid(selGridInt, selStrings, 1);
+       
+        GUILayout.EndScrollView();
+    }
+}
